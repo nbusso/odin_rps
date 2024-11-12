f8f9d93c875d5f84856939316c7dbb699d6aebe7
Nicolas Busso <nicolasbusso@gmail.com>
2024-11-12 10:02:19 -0300

    made buttons on the ui work

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // global variables
 let choices = ["rock", "paper", "scissors"];
+let playerChoice = "none";
+const btnContainer = document.querySelector("#btn-container");
 
 // computer choice logic
 function getComputerChoice() {
@@ -8,22 +10,77 @@ function getComputerChoice() {
 }
 
 // human choice logic
+
+btnContainer.addEventListener("click", (e) => {
+  console.log(`Player selected: "${e.target.id}"`);
+  playerChoice = e.target.id;
+
+  playRound(getHumanChoice(), getComputerChoice());
+});
+
+// render
+
+//! -------------
+/* const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+
+const playerSelection = (e) => {
+  console.log(e.target.id);
+  return e.target;
+};
+
+rockBtn.addEventListener("click", playerSelection); */
+//! ------------
+
 function getHumanChoice() {
-  let selectedHand = prompt('Enter "rock", "paper" or "scissors"');
-
-  while (!choices.includes(selectedHand.toLowerCase())) {
-    selectedHand = prompt(
-      'Please, enter "rock", "paper" or "scissors", otherwise the game will not work'
-    ).toLowerCase();
-
-    return selectedHand.toLowerCase();
-  }
-
-  return selectedHand.toLowerCase();
+  return playerChoice;
 }
 
+function playRound(humanChoice, computerChoice) {
+  //roundNumber += 1;
+
+  // play round logic
+  if (humanChoice === "rock") {
+    if (computerChoice === "rock") {
+      console.log("DRAW!, both players selected ROCK!");
+      drawNumber += 1;
+      roundNumber -= 1;
+    } else if (computerChoice === "paper") {
+      console.log("You lose! PAPER beats ROCK!");
+      computerScore += 1;
+    } else {
+      console.log("You win! ROCK beats SCISSORS");
+      humanScore += 1;
+    }
+  } else if (humanChoice === "paper") {
+    if (computerChoice === "rock") {
+      console.log("You win! PAPER beats ROCK!");
+      humanScore += 1;
+    } else if (computerChoice === "paper") {
+      console.log("DRAW!, both players selected PAPER!");
+      drawNumber += 1;
+      roundNumber -= 1;
+    } else {
+      console.log("You lose! SCISSORS beats PAPER");
+      computerScore += 1;
+    }
+  } else {
+    if (computerChoice === "rock") {
+      console.log("You lose! ROCK beats SCISSORS!");
+      computerScore += 1;
+    } else if (computerChoice === "paper") {
+      console.log("You win! SCISSORS beats PAPER!");
+      humanScore += 1;
+    } else {
+      console.log("DRAW!, both players selected SCISSORS!");
+      drawNumber += 1;
+      roundNumber -= 1;
+    }
+  }
+}
 // play full game logic
-function playGame() {
+/* function playGame() {
   let humanScore = 0;
   let computerScore = 0;
   let roundNumber = 0;
@@ -92,3 +149,5 @@ function playGame() {
 }
 
 playGame();
+
+ */
